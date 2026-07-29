a49ed4c7f7b3b8700e0ad382cf6c8816e007f7a4
abhinay mhatre <abhinaymhatre1234@gmail.com>
2026-07-30 01:30:15 +0530

    initial setup of tanstack query client is completed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,9 +5,13 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
 import AppRoutes from './app/routes/AppRoutes'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/queryClient'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
     <AppRoutes />
+    </QueryClientProvider>
   </Provider>,
 )
