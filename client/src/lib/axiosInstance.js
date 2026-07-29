b0954c9542637dddf495cec95920b813de4e0714
abhinay mhatre <abhinaymhatre1234@gmail.com>
2026-07-30 02:37:57 +0530

    added api for login register and getme

--- client/src/lib/axiosInstance.js
+++ client/src/lib/axiosInstance.js
@@ -1,6 +1,18 @@
+import { store } from "@/app/store";
+import { removeUser } from "@/features/auth/state/authSlice";
 import axios from "axios";
 
 export let axiosInstance = axios.create({
-    baseURL:'http://localhost:3000/api',
-    withCredentials:true,
+  baseURL: "http://localhost:3000/api",
+  withCredentials: true,
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      store.dispatch(removeUser());
+    }
+    return Promise.reject(error);
+  },
+);
