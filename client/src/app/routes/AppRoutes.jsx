2801f6a72dd7268ce36f86b877385b3f4808ab83
abhinay mhatre <abhinaymhatre1234@gmail.com>
2026-07-29 23:41:20 +0530

    added login,register and dashboard routes and configure tailwindcss

--- client/src/app/routes/AppRoutes.jsx
+++ client/src/app/routes/AppRoutes.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PublicRoute from "../protectedRoutes/PublicRoute";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "@/features/auth/pages/Login";
+import Register from "@/features/auth/pages/Register";
+import ProtectedRoute from "../protectedRoutes/ProtectedRoute";
+import DashboardLayout from "../layouts/DashboardLayout";
+import Dashboard from "@/features/dashboard/pages/Dashboard";
 
 const AppRoutes = () => {
-  return (
-    <div>AppRoutes</div>
-  )
-}
+  let router = createBrowserRouter([
+    {
+      path: "/",
+      element: <PublicRoute />,
+      children: [
+        {
+          path: "",
+          element: <AuthLayout />,
+          children: [
+            {
+              path: "",
+              element: <Login />,
+            },
+            {
+              path: "register",
+              element: <Register />,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      path: "/dashboard",
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: "",
+          element: <DashboardLayout />,
+          children: [
+            {
+              path: "",
+              element: <Dashboard />,
+            },
+          ],
+        },
+      ],
+    },
+  ]);
 
-export default AppRoutes
+  return <RouterProvider router={router}/>;
+};
+
+export default AppRoutes;
